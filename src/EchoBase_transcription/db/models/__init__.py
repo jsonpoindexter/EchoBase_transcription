3a44a8ed2ef56fb920109c62ff46abc6022e2b53
Jason Poindexter <poindexter.json@gmail.com>
2025-10-30 15:30:58 -0700

    Set up SQLModel metadata and create initial database tables

--- src/EchoBase_transcription/db/models/__init__.py
+++ src/EchoBase_transcription/db/models/__init__.py
@@ -1,15 +1,29 @@
 """SQLAlchemy base metadata and public model re‑exports."""
+from sqlalchemy import MetaData
+from sqlmodel import SQLModel
 
-from sqlalchemy.orm import DeclarativeBase
 
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
 
-class Base(DeclarativeBase):
-    """Project‑wide declarative base class."""
-    pass
-
+# Set the global metadata used by SQLModel BEFORE any models import
+SQLModel.metadata = MetaData(naming_convention=NAMING_CONVENTION)
 
 from .system import System
 from .talkgroup import TalkGroup
 from .radio_unit import RadioUnit
 from .call import Call
 from .user import User
+
+__all__ = [
+    "System",
+    "TalkGroup",
+    "RadioUnit",
+    "Call",
+    "User",
+]
